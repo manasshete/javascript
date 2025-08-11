dfd8684d568742c2b370dc7e1abd8fd48a73ed6e
manas shete <manasshete2006@gmail.com>
2025-08-11 21:49:36 +0530

    Sfv

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+
+let accountEmail = "manas@gmail.com";
+var accountPassword = "12345";
+accountCity = "New York";
+let accountState;
+//accountId = 2;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer no to use var 
+because of issue in block scope and functional scope
+*/
